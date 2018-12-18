a0004ae0587898eafd40667adb8e0961f9c2abde
Alexander McKinney <alexander.f.mckinney@durham.ac.uk>
2018-12-18 00:07:08 +0000

    Initial commit. 92% accurate I think

---
+++ eval.py
@@ -0,0 +1,83 @@
+from model import CNN
+
+import torch
+print("Starting Torch version:", torch.__version__)
+import torch.nn as nn
+import torch.nn.functional as F
+
+from tqdm import tqdm
+
+import torchvision
+print("Starting Torchvision version:", torchvision.__version__)
+#import torchvision.datasets
+from torchvision.datasets import FashionMNIST
+
+import matplotlib
+import matplotlib.pyplot as plt
+print("Starting matplotlib version:", matplotlib.__version__)
+
+# Get test set if they do not exist
+test_set = FashionMNIST(root="", train=False, download=True)
+
+# Print statistics about datasets
+print("MNIST loaded. Test:", len(test_set))
+
+# Load model
+print("Loading CNN from file.")
+cnn = torch.load("saved_model.pth")
+print(cnn)
+print("Loaded CNN.")
+
+print("Evaluating network")
+cnn.eval()
+correct = 0
+total = 0
+
+inputs, labels = torch.load("processed/test.pt") # Load training data
+torch.stack([inputs], dim=1, out=inputs) # Explode 2D image to 3D for conv layers (Clean up exploding later)
+inputs = inputs.type('torch.FloatTensor')
+
+# TODO: Parallelise evaluation
+with torch.no_grad():
+    for _ in tqdm(range(len(inputs))):
+        input = torch.stack([inputs[_]], dim=1, out=inputs[_]) # Clean this up later
+        output = cnn.forward(input)
+        prediction = torch.argmax(output)
+        # To do, have current evaluation in description of progress bar
+        #tqdm.set_description("Prediction %s . Actual %s" % (prediction, labels[_]))
+        if prediction == labels[_]:
+            correct += 1
+        total += 1
+
+print("Evaluation complete. Network accuracy:", correct/total * 100, "%")
+
+dictionary = {
+    0 : "T-Shirt/Top",
+    1 : "Trouser",
+    2 : "Pullover",
+    3 : "Dress",
+    4 : "Coat",
+    5 : "Sandal",
+    6 : "Shirt",
+    7 : "Sneaker",
+    8 : "Bag",
+    9 : "Ankle Boot"
+}
+
+for _ in range(16):
+    plt_input = torch.stack([inputs[_]], dim=1, out=inputs[_])
+
+    plt_prediction = torch.argmax(cnn(plt_input))
+
+    plt_input = plt_input.view((28, 28))
+
+    plt.subplot(4, 4, _+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.title(dictionary[int(plt_prediction)], fontsize=6)
+    plt.imshow(plt_input, cmap='gray')
+
+plt.axis('off')
+plt.tight_layout()
+plt.show()
+
---
+++ train.py
@@ -0,0 +1,64 @@
+from model import CNN
+
+import torch
+print("Starting Torch version:", torch.__version__)
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+from tqdm import tqdm
+
+import torchvision
+print("Starting Torchvision version:", torchvision.__version__)
+#import torchvision.datasets
+from torchvision.datasets import FashionMNIST
+
+# Get datasets if they do not exist
+train_set = FashionMNIST(root= "", download=True)
+
+# Print statistics about datasets
+print("MNIST loaded. Train:", len(train_set))
+
+cnn = CNN()
+
+input, t = torch.load("processed/training.pt") # Load training data
+
+target = torch.zeros(60000, 10)
+for _ in range(60000):
+    target[_, t[_]] = 1 # Explode labels into tensors
+
+torch.stack([input], dim=1, out=input) # Explode 2D image to 3D for conv layers
+
+# Convert to FloatTensor for use with conv layers
+# May be pointless to change type here if also changed at input_batch etc.
+input = input.type('torch.FloatTensor')
+target = target.type('torch.FloatTensor')
+
+print("Defining critic.")
+critic = nn.MSELoss() # Mean Square Error Loss
+print("Done")
+print("Defining Optimiser")
+optimiser = optim.Adam(cnn.parameters(), lr=0.001) # Adam Optimiser
+print("Done")
+
+bs=20 # Batch Size
+epoch=10 # Nb. Epochs
+print("Begin training")
+
+with tqdm(total=epoch*len(train_set)/bs) as progress_bar: # Create progress bar object
+    for _ in (range(epoch)): # Iterate across number of epochs
+        for b in (range(int(len(train_set)/bs))): # Iterate across number of batches
+            progress_bar.update(1) # Increment progress bar
+
+            # Get batch and convert to FloatTensor
+            input_batch = input[b*bs:(b+1)*bs].type('torch.FloatTensor')
+            target_batch = target[b*bs:(b+1)*bs].type('torch.FloatTensor')
+
+            optimiser.zero_grad() # Zero gradients
+            output = cnn(input_batch) # Get output from network
+            loss = critic(output, target_batch) # Calculate Loss
+            loss.backward() # Backpropogate errors
+            optimiser.step() # Apply changes to parameters
+
+print("Training Complete.")
+torch.save(cnn, "saved_model.pth")
